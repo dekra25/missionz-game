989fe48cce2a0b76df36643750f4a94a2c0c98af
Nishanta Deka <nishantadeka@Nishantas-MacBook-Air.local>
2026-03-13 14:34:21 +0530

    add requirements for render

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   server: {
+    host: '0.0.0.0',
     port: 5173,
-  },
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      '.ngrok-free.dev'
+    ]
+  }
 })
